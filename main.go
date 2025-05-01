3f9acacd7461ba98a7137dbec4af1614c1c7ed3f
Uthsob Chakraborty <uthsob9@gmail.com>
2025-05-02 02:58:36 +0600

    Readme file added, root and some modification

--- main.go
+++ main.go
@@ -73,7 +73,19 @@ func main() {
 		go RunScraper(db)
 		c.JSON(http.StatusOK, gin.H{"message": "Scraper started in background"})
 	})
-
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(302, "https://github.com/uthsobcb/UniDirectory")
+	})
+	router.GET("/health", func(c *gin.Context) {
+		router.GET("/status", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"schemaVersion": 1,
+				"label":         "status",
+				"message":       "UP",
+				"color":         "brightgreen",
+			})
+		})
+	})
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
