a4407bdf8f1672de06364aff12b7fd2ee70b955b
Uthsob Chakraborty <uthsob9@gmail.com>
2025-05-02 01:57:35 +0600

    De-Containerized and used cloud postgreqsl

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -15,14 +14,7 @@ import (
 var db *gorm.DB
 
 func initDB() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	dsn := os.Getenv("DB_URL")
 
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
